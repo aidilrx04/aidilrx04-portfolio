0d71ed997e3f47305a28eb4a59b73a7de1bcf1a0
aidilrx04 <mrcrayv14@gmail.com>
2023-10-16 23:02:59 +0800

    feat: add dropdown to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -29,7 +29,7 @@
 	use:responsiveNav
 	class="bg-slate-800 mx-auto my-4 mb-12 justify-between items-center px-9 transition-all z-50 sticky top-0 left-0
 	{shouldSticky ? 'w-full' : 'container lg:max-w-screen-lg '}
-	flex flex-col pt-4 sm:pt-0 sm:flex-row
+	flex relative
 	"
 >
 	<a href="/" class="brand">
@@ -39,22 +39,56 @@
 			Aidil.dev
 		</h1>
 	</a>
-	<ul class="flex gap-0">
-		<li>
-			<a class="nav-item" href="/">Home</a>
-		</li>
-		<li>
-			<a href="#about" class="nav-item">About</a>
-		</li>
-		<li>
-			<a href="#porfolio" class="nav-item">Portfolio</a>
-		</li>
-	</ul>
+	<div class="dropdown bg-inherit group">
+		<button
+			class="peer md:hidden w-full px-3 py-1 border-2 border-slate-700 rounded-lg my-4 flex items-center justify-center"
+		>
+			<i class="ph ph-list text-3xl text-slate-50" />
+		</button>
+		<!-- TODO: fix the navbar transition when dropdown is open -->
+		<div
+			class="max-h-0 h-auto md:max-h-full md:static md:block hidden dropdown-content absolute bg-inherit w-full left-0 top-full
+            peer-active:block
+            peer-focus:block
+            peer-focus-within:block
+            group-focus-within:block
+            group-focus:block
+            group-active:block
+            group-focus-within:max-h-screen
+            group-focus:max-h-screen
+            group-active:max-h-screen
+            peer-active:max-h-screen
+            peer-focus:max-h-screen
+            peer-focus-within:max-h-screen
+            transition-all duration-1000"
+		>
+			<ul class="md:flex md:justify-end gap-0 flex-wrap justify-center">
+				<li>
+					<a class="nav-item" href="/">Home</a>
+				</li>
+				<li>
+					<a href="#about" class="nav-item">About</a>
+				</li>
+				<li>
+					<a href="#porfolio" class="nav-item">Portfolio</a>
+				</li>
+				<li>
+					<a href="#contact" class="nav-item">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
 </nav>
 
-<style>
+<style lang="postcss">
 	.nav-item {
-		@apply block px-4 py-4 uppercase text-lg tracking-wider transition-all;
+		@apply block  py-4 uppercase text-lg tracking-wider transition-all px-9;
+	}
+
+	@media screen(md) {
+		.nav-item {
+			@apply px-4;
+		}
 	}
 
 	.nav-item:hover {
