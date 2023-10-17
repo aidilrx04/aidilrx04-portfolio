a75704c81b6e16755562c4da20a914eeaadbc62d
aidilrx04 <mrcrayv14@gmail.com>
2023-10-17 15:44:33 +0800

    feat: make section support lightmode

--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -1,3 +1,3 @@
-<section {...$$props} class="px-9 py-9 mb-12 bg-slate-800 {$$props?.class}">
+<section {...$$props} class="px-9 py-9 mb-12 bg-slate-50 dark:bg-slate-800 {$$props?.class}">
 	<slot />
 </section>
