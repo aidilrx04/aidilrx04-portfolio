69faa9b8b2fe49007a403ce88795ceb375553721
aidilrx04 <mrcrayv14@gmail.com>
2023-10-17 16:01:34 +0800

    fix: force light mode even if prefered scheme dark

--- src/lib/color-scheme.ts
+++ src/lib/color-scheme.ts
@@ -6,12 +6,13 @@ export const darkMode = writable(false);
 if (browser) {
 	console.log('runned');
 
-	const mode =
-		(localStorage.getItem('darkMode')
-			? localStorage.getItem('darkMode') === 'dark'
-				? true
-				: false
-			: false) || window.matchMedia('(prefers-color-scheme: dark)').matches;
+	const mode = localStorage.getItem('darkMode')
+		? localStorage.getItem('darkMode') === 'dark'
+			? true
+			: false
+		: window.matchMedia('(prefers-color-scheme: dark)').matches
+		? true
+		: false;
 
 	darkMode.set(mode);
 }
